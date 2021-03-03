c2da72fe24701725f39a36ecb708a06a332f8560
mascott88 <mas4u19@gmail.com>
2021-03-03 13:12:06 -0500

    Loops

---
+++ LoopAssignments/LoopsAssignment2.java
@@ -0,0 +1,23 @@
+
+
+public class LoopsAssignment2 {
+	
+	public static void main(String args[]) {
+		// 2)Print all the palindrome numbers between 100 and 999
+		int x = 100;
+		while (x < 1000) {
+			String input = Integer.toString(x).substring(0);
+			String palindrome = reverse(input);
+			if(input.equals(palindrome)) {
+				System.out.println(x);
+			}
+			x++;
+		}
+	}
+	private static String reverse(String input) {
+		if (input.length() <= 1) {			
+			return input;
+		}
+		return reverse(input.substring(1)) + input.substring(0, 1);
+	}
+}
